a4ab183a634cb5e006357a106e06ac866df6de64
sercantimocin <sercantimocin@gmail.com>
2018-11-02 19:21:08 +0300

    has all characters

---
+++ HasAllCharctersUnique/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace HasAllCharctersUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = HasAllCharacters("AAAAAABCDEFGHIJLMNOPQRSTUVWXYZZZZZ");
+            Console.WriteLine(r);
+            var k = Console.ReadKey();
+        }
+
+
+        //1.1 Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
+        public static bool HasAllCharacters(string text)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] existChars = new char[26];
+
+            foreach (char t in text.ToUpper())
+            {
+                existChars[t % 65] = t;
+            }
+
+            return alphabet.SequenceEqual(existChars);
+        }
+    }
+}
