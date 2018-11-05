30e7d45b3cd2ddc6a957dd7c3f7dcf053ffcd92f
sercantimocin <sercantimocin@gmail.com>
2018-11-05 10:39:07 +0300

    move seperate class 1.1 and 1.2

--- HasAllCharctersUnique/Program.cs
+++ HasAllCharctersUnique/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using ArrayAndStrings;
 
 namespace HasAllCharctersUnique
 {
@@ -7,38 +8,19 @@ namespace HasAllCharctersUnique
     {
         static void Main(string[] args)
         {
+            ISolutionLib solution = new SolutionLib();
+
             Console.WriteLine("1.1");
-            var r = HasAllCharacters("AAAAAABCDEFGHIJLMNOPQRSTUVWXYZZZZZ");
+            var r = solution.HasAllCharacters_1_1("AAAAAABCDEFGHIJLMNOPQRSTUVWXYZZZZZ");
             Console.WriteLine(r);
 
             Console.WriteLine("1.2");
-            ReverseStr("sercan");
+            solution.ReverseStr_1_2("sercan");
 
             var k = Console.ReadKey();
         }
 
 
-        //1.1 Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
-        public static bool HasAllCharacters(string text)
-        {
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            char[] existChars = new char[26];
 
-            foreach (char t in text.ToUpper())
-            {
-                existChars[t % 65] = t;
-            }
-
-            return alphabet.SequenceEqual(existChars);
-        }
-
-        //1.2 Write code to reverse a C-Style String(C-String means that “abcd” is represented as five characters, including the null character )
-        public static void ReverseStr(string str)
-        {
-            for (int i = (str.Length-1); i >= 0; i--)
-            {
-                Console.Write(str[i]);
-            }
-        }
     }
 }
---
+++ HasAllCharctersUnique/ResolutionLib.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayAndStrings
+{
+    public interface ISolutionLib
+    {
+        bool HasAllCharacters_1_1(string text);
+        void ReverseStr_1_2(string str);
+    }
+
+    public class SolutionLib : ISolutionLib
+    {
+        //1.1 Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
+        public bool HasAllCharacters_1_1(string text)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] existChars = new char[26];
+
+            foreach (char t in text.ToUpper())
+            {
+                existChars[t % 65] = t;
+            }
+
+            return alphabet.SequenceEqual(existChars);
+        }
+
+        //1.2 Write code to reverse a C-Style String(C-String means that “abcd” is represented as five characters, including the null character )
+        public void ReverseStr_1_2(string str)
+        {
+            for (int i = (str.Length - 1); i >= 0; i--)
+            {
+                Console.Write(str[i]);
+            }
+        }
+    }
+}
