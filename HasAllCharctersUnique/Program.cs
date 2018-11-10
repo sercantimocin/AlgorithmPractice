b5a6116471c6442ba71917458fa2ac52dd8b6ef3
sercantimocin <sercantimocin@gmail.com>
2018-11-10 22:19:31 +0300

    1.3

--- HasAllCharctersUnique/Program.cs
+++ HasAllCharctersUnique/Program.cs
@@ -11,16 +11,16 @@ namespace HasAllCharctersUnique
             ISolutionLib solution = new SolutionLib();
 
             Console.WriteLine("1.1");
-            var r = solution.HasAllCharacters_1_1("AAAAAABCDEFGHIJLMNOPQRSTUVWXYZZZZZ");
+            var r = solution.IsUniqueAllChars_1_1("sercan");
             Console.WriteLine(r);
 
             Console.WriteLine("1.2");
             solution.ReverseStr_1_2("sercan");
 
+            Console.WriteLine("1.3");
+            solution.RemoveDuplicateChars_1_3("sercean");
+
             var k = Console.ReadKey();
         }
-
-
-
     }
 }
--- HasAllCharctersUnique/ResolutionLib.cs
+++ HasAllCharctersUnique/ResolutionLib.cs
@@ -7,24 +7,30 @@ namespace ArrayAndStrings
 {
     public interface ISolutionLib
     {
-        bool HasAllCharacters_1_1(string text);
+        bool IsUniqueAllChars_1_1(string text);
         void ReverseStr_1_2(string str);
+        string RemoveDuplicateChars_1_3(string text);
     }
 
     public class SolutionLib : ISolutionLib
     {
         //1.1 Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
-        public bool HasAllCharacters_1_1(string text)
+        public bool IsUniqueAllChars_1_1(string text)
         {
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            char[] existChars = new char[26];
+            bool[] chars = new bool[256];
 
-            foreach (char t in text.ToUpper())
+            foreach (char chr in text)
             {
-                existChars[t % 65] = t;
+                int asciiCode = (int)chr;
+                if (chars[asciiCode])
+                {
+                    return false;
+                }
+
+                chars[asciiCode] = true;
             }
 
-            return alphabet.SequenceEqual(existChars);
+            return true;
         }
 
         //1.2 Write code to reverse a C-Style String(C-String means that “abcd” is represented as five characters, including the null character )
@@ -34,6 +40,36 @@ namespace ArrayAndStrings
             {
                 Console.Write(str[i]);
             }
+
+            Console.WriteLine();
+        }
+
+        // 1.3 
+        // Design an algorithm and write code to remove the duplicate characters in a string without using any additional buﬀer NOTE: One or two additional variables are fne
+        // An extra copy of the array is not
+        // FOLLOW UP
+        // Write the test cases for this method
+        public string RemoveDuplicateChars_1_3(string text)
+        {
+            int charLength = text.Length - 1;
+            int currentIndex=0;
+
+            for (int i = 0; i < charLength; i++)
+            {
+                char currentChar = text[i];
+                for (int j = i + 1; j < charLength; j++)
+                {
+                    char comparingChar = text[j];
+                    if (currentChar == comparingChar)
+                    {
+                        break;
+                    }
+
+                    currentIndex++;
+                }
+            }
+
+            return text;
         }
     }
 }
